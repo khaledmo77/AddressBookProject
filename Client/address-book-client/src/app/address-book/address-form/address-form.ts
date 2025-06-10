5ab44f9d4c13203341c16efaeb93fe122d407ee9
khaledmo77 <khaledmo77@outlook.com>
2025-06-10 16:47:58 +0300

    added addressbook module

---
+++ Client/address-book-client/src/app/address-book/address-form/address-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-address-form',
+  imports: [],
+  templateUrl: './address-form.html',
+  styleUrl: './address-form.scss'
+})
+export class AddressForm {
+
+}
