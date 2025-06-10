99946a8aaef8b6e9e8072a83353e257df8fd78d5
khaledmo77 <khaledmo77@outlook.com>
2025-06-10 16:48:30 +0300

    delete popup module

---
+++ Client/address-book-client/src/app/address-book/delete-confirm/delete-confirm.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirm',
+  imports: [],
+  templateUrl: './delete-confirm.html',
+  styleUrl: './delete-confirm.scss'
+})
+export class DeleteConfirm {
+
+}
