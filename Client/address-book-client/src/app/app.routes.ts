4f1dedf577c5190bddcf3638b6b22dc90775291c
khaledmo77 <khaledmo77@outlook.com>
2025-06-10 16:47:28 +0300

    installed necessary lib and modified routng

--- Client/address-book-client/src/app/app.routes.ts
+++ Client/address-book-client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { AddressListComponent } from "./components/address-list/address-list.component";
+import { JobListComponent } from "./components/job-list/job-list.component";
+import { DepartmentListComponent } from "./components/department-list/department-list.component";
+import { AuthGuard } from "./guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard], // optional
+    children: [
+      { path: 'addresses', component: AddressListComponent },
+      { path: 'jobs', component: JobListComponent },
+      { path: 'departments', component: DepartmentListComponent },
+    ]
+  },
+];
